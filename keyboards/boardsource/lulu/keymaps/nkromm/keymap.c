fede1794abcb28fec5e53cbf3f3e5e2ce07ead0b
Nikolaos Krommydas <nkrommydas@fastmail.com>
2022-11-19 09:11:40 +0100

    fix typos in qwerty & gaming, put symbols & * on 1st layer

--- keyboards/boardsource/lulu/keymaps/nkromm/keymap.c
+++ keyboards/boardsource/lulu/keymaps/nkromm/keymap.c
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_COLEMAK] = LAYOUT(
-  KC_ESC,  KC_EXLM,  KC_AT,  KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,  KC_BSPC,
+  KC_ESC,  KC_EXLM,  KC_AT,  KC_HASH, KC_DLR,  KC_AMPR,                            KC_ASTR, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,  KC_BSPC,
   KC_TAB,  KC_Q,     KC_W,   KC_F,    KC_P,    KC_B,                               KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,  KC_BSLS,
   KC_QUOT, MOD_A,    MOD_R,  MOD_S,   MOD_T,   KC_G,                               KC_M,    MOD_N,   MOD_E,   MOD_I,   MOD_O,    KC_MINS,
   KC_GRV,  KC_Z,     KC_X,   KC_C,    KC_D,    KC_V,    KC_MPLY,         RGB_TOG,  KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,  KC_EQL,
@@ -65,10 +65,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
  [_QWERTY] = LAYOUT(
-  KC_ESC,  KC_EXLM,  KC_AT,  KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,  KC_BSPC,
+  KC_ESC,  KC_EXLM,  KC_AT,  KC_HASH, KC_DLR,  KC_AMPR,                            KC_ASTR, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,  KC_BSPC,
   KC_TAB,  KC_Q,   KC_W,   KC_E,    KC_R,    KC_T,                                 KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_BSLS,
   KC_QUOT, MOD_A,  MOD_S,  MOD_D,   MOD_F,   KC_G,                                 KC_H,    MOD_J,   MOD_K,   MOD_L,   MOD_SCLN, KC_MINS,
-  KC_GRV,  KC_Z,   KC_X,   KC_C,    KC_D,    KC_V,    KC_MPLY,           RGB_TOG,  KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,  KC_EQL,
+  KC_GRV,  KC_Z,   KC_X,   KC_C,    KC_V,    KC_B,    KC_MPLY,           RGB_TOG,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_EQL,
                            ADJUST,  KC_LBRC, KC_RBRC, LT_RSSP,           LT_RSRT,  KC_LPRN, KC_RPRN, ADJUST
 ),
 
@@ -87,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `----------------------------'           '------''--------------------'
  */
  [_RAISE] = LAYOUT(
-  KC_ESC,   LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), KC_AMPR,                          KC_ASTR, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_BSPC,
+  KC_ESC,   LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), KC_PERC,                          KC_CIRC, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_BSPC,
   KC_DEL,   KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
   KC_INS,   _______,    _______,    LCTL(KC_S), _______,    _______,                          _______, _______, _______, _______, _______, KC_INS,
   KC_F1,    KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,     _______,      _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
@@ -106,7 +106,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,  KC_1,  KC_2,  KC_3,    KC_4,     KC_5,                                    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC,
   KC_TAB,  KC_Q,  KC_W,  KC_E,    KC_R,     KC_T,                                    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_BSLS,
   KC_QUOT, KC_A,  KC_S,  KC_D,    KC_F,     KC_G,                                    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_MINS,
-  KC_GRV,  KC_Z,  KC_X , KC_C,    KC_D,     KC_V,    COLEMAK,              COLEMAK,  KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,  KC_EQL,
+  KC_GRV,  KC_Z,  KC_X , KC_C,    KC_V,     KC_B,    COLEMAK,              COLEMAK,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_EQL,
                          KC_LALT, KC_LCTRL, KC_LSFT, KC_SPC,               LT_RSRT,  KC_LPRN, KC_RPRN, KC_CAPS
 ),
 
