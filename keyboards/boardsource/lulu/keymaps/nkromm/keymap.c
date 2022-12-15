a0f81dc9d430957907e648125aa373bafdfedb66
Nikolaos Krommydas <nkrommydas@fastmail.com>
2022-12-15 11:50:12 +0100

    fix typo on LCTRL to LCTL

--- keyboards/boardsource/lulu/keymaps/nkromm/keymap.c
+++ keyboards/boardsource/lulu/keymaps/nkromm/keymap.c
@@ -107,7 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,  KC_W,  KC_E,    KC_R,     KC_T,                                    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_BSLS,
   KC_QUOT, KC_A,  KC_S,  KC_D,    KC_F,     KC_G,                                    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_MINS,
   KC_GRV,  KC_Z,  KC_X , KC_C,    KC_V,     KC_B,    COLEMAK,              COLEMAK,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_EQL,
-                         KC_LALT, KC_LCTRL, KC_LSFT, KC_SPC,               LT_RSRT,  KC_LPRN, KC_RPRN, KC_CAPS
+                         KC_LALT, KC_LCTL, KC_LSFT, KC_SPC,               LT_RSRT,  KC_LPRN, KC_RPRN, KC_CAPS
 ),
 
 };
